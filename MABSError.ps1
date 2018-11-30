61ec2e04a0bd56e1fd826caad793c4854532bc77
Alex Innes <alexnixoninnes@gmail.com>
2018-11-30 13:17:22 +0000

    added some comments for clarity

--- MABSError.ps1
+++ MABSError.ps1
@@ -1,12 +1,16 @@
 #requires -RunAsAdministrator
+
+#needs to be run on a box with DPM/MABS
 Import-Module "DataProtectionManager"
 
 $getJobs = Get-DPMJob
 
+#gets the failed jobs - might update for all jobs so can filter via function
 $failedJobs = $getJobs | where {$_.status -eq "Failed"}
 
 $failarray = @()
 
+#loop jobs and pull relevant info
 foreach ($failedjob in $failedJobs){
     $failObject = [ordered]@{
         Status = $failedJob.status
@@ -16,7 +20,7 @@ foreach ($failedjob in $failedJobs){
         StartTime = $failedjob.starttime
         EndTime = $failedjob.endtime
         Duration = (New-TimeSpan -Start $failedjob.starttime -End $failedjob.endtime)
-        
+
     }
     $failArray += $failObject
 }
